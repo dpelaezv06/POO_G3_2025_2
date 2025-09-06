3806a30b00f0a083d089e3cf5e39546306da323f
dpelaezv06 <dpelaezv@unal.edu.co>
2025-09-06 12:25:47 -0500

    creacion del proyecto para el ejercicio 4

---
+++ actividad_1/ejercicio_resuelto4/src/main/java/com/mycompany/ejercicio_resuelto4/Ejercicio_resuelto4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio_resuelto4;
+
+/**
+ *
+ * @author daniel
+ */
+public class Ejercicio_resuelto4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
