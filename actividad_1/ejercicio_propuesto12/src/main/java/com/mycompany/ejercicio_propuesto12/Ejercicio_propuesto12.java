97e1e7f65b5813f80de957e585dafbeb0e184317
dpelaezv06 <dpelaezv@unal.edu.co>
2025-09-06 20:40:54 -0500

    creacion del proyeecto apra el ejercicio propuesto 12

---
+++ actividad_1/ejercicio_propuesto12/src/main/java/com/mycompany/ejercicio_propuesto12/Ejercicio_propuesto12.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio_propuesto12;
+
+/**
+ *
+ * @author daniel
+ */
+public class Ejercicio_propuesto12 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
