d9907d8f6c9099a08bd08019e2db2075bc2c335d
dpelaezv06 <dpelaezv@unal.edu.co>
2025-09-06 19:52:24 -0500

    creacion del proyecto para el ejercicio 5

---
+++ actividad_1/ejercicio_resuleto5/src/main/java/com/mycompany/ejercicio_resuleto5/Ejercicio_resuleto5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio_resuleto5;
+
+/**
+ *
+ * @author daniel
+ */
+public class Ejercicio_resuleto5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
