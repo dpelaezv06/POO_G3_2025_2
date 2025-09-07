b542a07c1aacfcba5fed548ecd049601472fb88c
dpelaezv06 <dpelaezv@unal.edu.co>
2025-09-06 20:25:41 -0500

    main con el ejercicio resuelto

--- actividad_1/ejercicio_resuleto5/src/main/java/com/mycompany/ejercicio_resuleto5/Ejercicio_resuleto5.java
+++ actividad_1/ejercicio_resuleto5/src/main/java/com/mycompany/ejercicio_resuleto5/Ejercicio_resuleto5.java
@@ -11,6 +11,19 @@ package com.mycompany.ejercicio_resuleto5;
 public class Ejercicio_resuleto5 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        /* declaracion de variables */
+        double suma = 0; // variable para almacenar la suma inicializada en 0
+        double x = 20; // variable x inicializada en 20
+        double y = 0; // variable y inicializada en 0
+
+        /* se realiza el seguimiento */
+        suma = 0; // suma inicia en 0
+        suma = calculos.sumar(suma, x); // suma = 0 + 20
+        y = 40; // y = 40
+        x = calculos.calcular_x(x, y); // x = 20 + 40^2 = 1620
+        suma = calculos.sumar(suma, x/y); // suma = 20 + 1620/40 = 60.5
+        
+        /* se muestra el resultado final */
+        System.out.println("El valor final es: " + suma); // se imprime el resultado
     }
 }
