8ad264b33b8e7dae2a4fc3f587e3dd9eb90ac76b
dpelaezv06 <dpelaezv@unal.edu.co>
2025-09-08 11:41:33 -0500

    creacion de la clase calculos
    En esta clase se almacenan los metodos necesarios para calcular el perimetro y el area. Ademas de una funcion extra para cambiar las comas por puntos y poder recibir ambos como separador decimal

--- actividad_1/ejercicio_propuesto17/src/main/java/com/mycompany/ejercicio_propuesto17/calculos.java
+++ actividad_1/ejercicio_propuesto17/src/main/java/com/mycompany/ejercicio_propuesto17/calculos.java
@@ -9,5 +9,25 @@ package com.mycompany.ejercicio_propuesto17;
  * @author daniel
  */
 public class calculos {
+    static double area_circulo(double radio){
+        /* metodo que permite calcular el area de un circulo segun su radio */
+        double area; // variable que almacena el area del circulo
+        area = Math.PI * Math.pow(radio, 2); // formula para calcular el area del circulo
+        return area;
+    }
+
+    static double perimetro_circulo(double radio){
+        /* metodo que permite calcular el perimetro de un circulo segun su radio */
+        double perimetro; // variable que almacena el perimetro del circulo
+        perimetro = 2 * Math.PI * radio; // formula para calcular el perimetro del circulo
+        return perimetro;
+    }
+
+    static String comas_porPuntos(String entrada) {
+        /* metodo para reemplazar las comas por puntos para que ambos seeparadores decimales sean aceptados */
+        String salida;
+        salida = entrada.replace(',', '.');
+        return salida;
+    }
     
 }
