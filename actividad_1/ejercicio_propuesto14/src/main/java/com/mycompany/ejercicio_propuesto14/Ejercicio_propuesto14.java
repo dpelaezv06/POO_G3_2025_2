3508909e46a29affd40f2f26b25d4df808f693ea
dpelaezv06 <dpelaezv@unal.edu.co>
2025-09-08 10:51:49 -0500

    creacion de proyecto parar solucionar ejercicio propuesto 14

---
+++ actividad_1/ejercicio_propuesto14/src/main/java/com/mycompany/ejercicio_propuesto14/Ejercicio_propuesto14.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio_propuesto14;
+
+/**
+ *
+ * @author daniel
+ */
+public class Ejercicio_propuesto14 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
