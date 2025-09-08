9fd0ebd41d94d4484499e276d3977a088bd1f0dd
dpelaezv06 <dpelaezv@unal.edu.co>
2025-09-08 11:41:42 -0500

    resolucion del ejercicio

--- actividad_1/ejercicio_propuesto17/src/main/java/com/mycompany/ejercicio_propuesto17/Ejercicio_propuesto17.java
+++ actividad_1/ejercicio_propuesto17/src/main/java/com/mycompany/ejercicio_propuesto17/Ejercicio_propuesto17.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.ejercicio_propuesto17;
 
+import java.util.Scanner;
+
 /**
  *
  * @author daniel
@@ -11,6 +13,26 @@ package com.mycompany.ejercicio_propuesto17;
 public class Ejercicio_propuesto17 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        /* primero se lee el numero ingresado por teclado */
+        double radio; // variable que almacena el numero ingresado por teclado
+        double perimetro; // variable para almacenar el perimetro del circulo
+        double area; // variable para almacenar el area del circulo
+        String entrada_string; // variable para almacenar el radio en forma de string
+        System.out.print("Ingrese un numero: "); // mensaje para el usuario
+        Scanner entrada = new Scanner(System.in); // objeto de la clase Scanner para leer por teclado
+        entrada_string = entrada.next(); // lectura del numero ingresado por teclado
+        entrada_string = calculos.comas_porPuntos(entrada_string); // se cambian las comas por puntos en el string para poder usar double
+
+        radio = Double.parseDouble(entrada_string); // se convierte la entrada a double para poder operar con ella       
+
+        /* se llaman los metodos para calcular cuadrados y cubos */
+        perimetro = calculos.perimetro_circulo(radio); // se calcula el perimetro del circulo
+        area = calculos.area_circulo(radio);         // se calcula el area del circulo
+
+        /* mostrar los resultados por consola */
+        System.out.println("El perimetro es " + perimetro); 
+        System.out.println("El area es " + area);
+
+        entrada.close(); // cerrar el objeto entrada
     }
 }
