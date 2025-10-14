fc1896f4a136865499dacc53359c061721a460a2
dpelaezv06 <dpelaezv@unal.edu.co>
2025-10-14 17:14:41 -0500

    Clase figura
    La clase contiene  objetos del tipo figura que tienen su superficie y su volumen

---
+++ actividad_3/ejercicio8_3/src/main/java/com/mycompany/ejercicio8_3/Figura.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejercicio8_3;
+
+/**
+ *
+ * @author daniel
+ */
+public class Figura {
+    private double superficie; // superficie de la figura
+    private double volumen; // volumen de la figura
+
+    public void setSuperficie(double superficie){
+        this.superficie = superficie;
+    }
+
+    public double getSuperficie(){
+        return this.superficie;
+    }
+
+    public void setVolumen(double volumen){
+        this.volumen = volumen;
+    }
+
+    public double getVolumen(){
+        return this.volumen;
+    }
+
+    
+
+
+    
+}
