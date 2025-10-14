fbf031085c49edd0c57aad65810b57c33fead123
dpelaezv06 <dpelaezv@unal.edu.co>
2025-10-14 11:56:19 -0500

    metodo para calcular el promedio

--- actividad_3/ejercicio8_2/src/main/java/com/mycompany/ejercicio8_2/Notas.java
+++ actividad_3/ejercicio8_2/src/main/java/com/mycompany/ejercicio8_2/Notas.java
@@ -9,5 +9,21 @@ package com.mycompany.ejercicio8_2;
  * @author daniel
  */
 public class Notas {
+    double[] lista_notas; // Array para almacenar las notas
+    
+    public Notas(){
+        lista_notas = new double[5]; // Inicializa el array con capacidad para 5 notas
+    }
+
+    double calcular_promedio(){
+        /* metodo para calcular el promedio de las notas */
+        double suma = 0; // variable para almacenar la suma de las notas
+        double promedio; // variable para almacenar el promedio
+        for (int contador = 1; contador < lista_notas.length; contador++) {
+            suma += lista_notas[contador]; // suma cada nota al total
+        }
+        promedio = suma / (lista_notas.length); // calcula el promedio
+        return promedio; // retorna el promedio
+    }
     
 }
