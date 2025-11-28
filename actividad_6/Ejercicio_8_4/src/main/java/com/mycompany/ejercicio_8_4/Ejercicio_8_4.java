4f55c998376d4999fc6a0db139a80963a774e09d
dpelaezv06 <dpelaezv@unal.edu.co>
2025-11-28 14:46:24 -0500

    creacion de proyecto para ejercicio 4.8

---
+++ actividad_6/Ejercicio_8_4/src/main/java/com/mycompany/ejercicio_8_4/Ejercicio_8_4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio_8_4;
+
+/**
+ *
+ * @author daniel
+ */
+public class Ejercicio_8_4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
