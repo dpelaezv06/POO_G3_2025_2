f94d3b2696b32b8c443bf4f033472e3a4053127a
dpelaezv06 <dpelaezv@unal.edu.co>
2025-11-29 12:07:30 -0500

    artibutos del objeto empleado

--- actividad_6/Ejercicio_8_4/src/main/java/com/mycompany/ejercicio_8_4/Empleado.java
+++ actividad_6/Ejercicio_8_4/src/main/java/com/mycompany/ejercicio_8_4/Empleado.java
@@ -10,4 +10,16 @@ package com.mycompany.ejercicio_8_4;
  */
 public class Empleado {
     
+    /* lista de atributos que posee el objeto empleado */
+    private String nombre; // nombre
+    private String apellidos; // apellidos del empleado
+    private double salario_diario; // salario que gana el emplado por dia
+    private double otros_ingresos; // otros ingresos que podria tener el empleado
+    private double pagos_salud; // pagos que realiza el empleado por salud
+    private double aporte_pensiones; // aportes del empleado a pensiones
+    private int dias_trabajados; // cantidad de ddias por mes que trabaja el empleado
+    private TipoCargo cargo; // tipo de cargo que tiene el empleado
+
+
+    
 }
