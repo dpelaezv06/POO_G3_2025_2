8a2e65be817738c5f876920e28a8cd8cd0dc1336
dpelaezv06 <dpelaezv@unal.edu.co>
2025-12-07 15:39:57 -0500

    Atributos y metodos de la clase habitacicon

--- actividad_6/Ejercicio_8_5/src/main/java/com/mycompany/ejercicio_8_5/Habitacion.java
+++ actividad_6/Ejercicio_8_5/src/main/java/com/mycompany/ejercicio_8_5/Habitacion.java
@@ -9,5 +9,41 @@ package com.mycompany.ejercicio_8_5;
  * @author daniel
  */
 public class Habitacion {
+    private int numero_habitacion;
+    private boolean disponible;
+    private double precio_dia;
+    private Huesped huesped;
+
+    public Habitacion(int numero_habitacion, boolean disponible, double precio_dia) {
+        this.numero_habitacion = numero_habitacion;
+        this.disponible = disponible;
+        this.precio_dia = precio_dia;
+    }
+
+    public int getNumeroHabitacion() {
+        return numero_habitacion;
+    }
+
+    public boolean getDisponible() {
+        return disponible;
+    }
+
+    public double getPrecioDia() {
+        return precio_dia;
+    }
+
+    public Huesped getHuesped() {
+        return huesped;
+    }
+
+    public void setHuesped(Huesped huesped) {
+        this.huesped = huesped;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    
     
 }
